8529135048f15ecbd3101176143704802a0277e1
Charlles Bandeira <charllesbandeira@gmail.com>
2018-11-19 20:01:49 -0200

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/nelioalves/cursomc/domain/Cidade.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cidade.java
@@ -18,6 +18,7 @@ public class Cidade implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
