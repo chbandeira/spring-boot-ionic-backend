066a9c667bfe8098a28ad761284dddac8bc0f363
Charlles Bandeira <charllesbandeira@gmail.com>
2018-12-03 18:21:03 -0200

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -61,6 +61,11 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria("Informática");
 		Categoria cat2 = new Categoria("Escritório");		
+		Categoria cat3 = new Categoria("Cama");
+		Categoria cat4 = new Categoria("Mesa");
+		Categoria cat5 = new Categoria("Banho");
+		Categoria cat6 = new Categoria("Design");
+		Categoria cat7 = new Categoria("Serviços");
 		
 		Produto p1 = new Produto("Computador", 2000.00);
 		Produto p2 = new Produto("Impressora", 800.00);
@@ -73,7 +78,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado("Minas Gerais");
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -26,8 +28,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 
-	@GetMapping(value="/{id}")
-	//@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}")
+	// @RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok(obj);
@@ -38,32 +40,44 @@ public class CategoriaResource {
 		obj = service.insert(obj);
 		return ResponseEntity.status(HttpStatus.CREATED).body(obj);
 	}
-	
+
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
-	/*@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
-		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	}*/
-	
-	@DeleteMapping(value= "/{id}")
+
+	/*
+	 * @RequestMapping(method = RequestMethod.POST) public ResponseEntity<Categoria>
+	 * insert(@RequestBody Categoria obj) { obj = service.insert(obj); URI uri =
+	 * ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+	 * .buildAndExpand(obj.getId()).toUri(); return
+	 * ResponseEntity.created(uri).build(); }
+	 */
+
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue="ASC") String direction) {
+		
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok(listDto);
 	}
 }
